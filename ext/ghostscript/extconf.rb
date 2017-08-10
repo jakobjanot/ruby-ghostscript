f5e01bfe62079c91933961a7936f5cd9990bef83
Jakob Kofoed Janot <jakob.janot@karnovgroup.com>
2017-08-10 16:51:08 +0200

    Fake makefile generation

--- ext/ghostscript/extconf.rb
+++ ext/ghostscript/extconf.rb
@@ -1,12 +1,24 @@
 require 'mkmf'
 
+find_executable('make')
+
 root = File.expand_path('../../..', __FILE__)
 
+# Create a dummy extension file. Without this RubyGems would abort the
+# installation process. On Linux this would result in the file "wat.so"
+# being created in the current working directory.
+#
+# Normally the generated Makefile would take care of this but since we
+# don't generate one we'll have to do this manually.
+#
+File.touch(File.join(File.dirname(__FILE__), "ghostscript.#{RbConfig::CONFIG['DLEXT']}"))
+
 Dir.chdir(File.join(root, 'vendor/ghostscript-9.21')) do
-  system "./configure", "--prefix=#{root} --disable-cups --disable-gtk --with-drivers=FILES"
-  system "make"
-  system "make install"
-  system "make clean"
+  system "./configure --prefix=#{root} --disable-cups --disable-gtk --with-drivers=FILES"
+  system "make && make install && make clean"
 end
 
-create_makefile 'ghostscript'
+# This is normally set by calling create_makefile() but we don't need that
+# method since we'll provide a dummy Makefile. Without setting this value
+# RubyGems will abort the installation.
+$makefile_created = true
